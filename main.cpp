31159972f314dbca0cb23528cc60c008481f3847
Neil Johnson <mr.neil.j@gmail.com>
2017-02-06 23:47:51 -0800

    Added planet string in

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
 // This program demonstrates the range-based for loop
+#include <iostream>
+#include <string>
+using  namespace std;
+
+int main()
+{
+    string planets[] = {"Mercury", "Venus", "Earth", "Mars",
+                        "Jupiter", "Saturn", "Uranus", "Neptune",
+                        "Pluto (a dwarf planet)" };
+
+    // Diaplsy the values in
+}
